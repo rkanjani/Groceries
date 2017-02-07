4ac1aca6019fdbcfc42a820413e92581eec4adbe
rkanjani <rkanj017@uottawa.ca>
2017-02-06 21:49:56 -0500

    Initial commit

---
+++ platforms/ios/build/Groceries.build/Debug-iphonesimulator/Groceries.build/Objects-normal/i386/main.d
@@ -0,0 +1,8 @@
+dependencies: \
+  /Users/rohan/Documents/Typescript/Groceries/platforms/ios/internal/main.m \
+  /Users/rohan/Documents/Typescript/Groceries/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/rohan/Documents/Typescript/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/rohan/Documents/Typescript/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/rohan/Documents/Typescript/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/rohan/Documents/Typescript/Groceries/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/rohan/Documents/Typescript/Groceries/platforms/ios/internal/TNSDebugging.h
---
+++ platforms/ios/build/emulator/Groceries.app/app/app.component.js
@@ -0,0 +1,15 @@
+"use strict";
+var core_1 = require("@angular/core");
+var AppComponent = (function () {
+    function AppComponent() {
+    }
+    return AppComponent;
+}());
+AppComponent = __decorate([
+    core_1.Component({
+        selector: "my-app",
+        template: "\n  <TextField hint=\"Email Address\" keyboardType=\"email\"\n  autocorrect=\"false\" autocapitalizationType=\"none\"></TextField>\n  <TextField hint=\"Password\" secure=\"true\"></TextField>\n  \n  <Button text=\"Sign in\"></Button>\n  <Button text=\"Sign up for Groceries\"><</Button>\n\n  "
+    })
+], AppComponent);
+exports.AppComponent = AppComponent;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLmNvbXBvbmVudC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImFwcC5jb21wb25lbnQudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLHNDQUEwQztBQWMxQyxJQUFhLFlBQVk7SUFBekI7SUFBMkIsQ0FBQztJQUFELG1CQUFDO0FBQUQsQ0FBQyxBQUE1QixJQUE0QjtBQUFmLFlBQVk7SUFaeEIsZ0JBQVMsQ0FBQztRQUNULFFBQVEsRUFBRSxRQUFRO1FBQ2xCLFFBQVEsRUFBRSxzU0FRVDtLQUNGLENBQUM7R0FDVyxZQUFZLENBQUc7QUFBZixvQ0FBWSIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IENvbXBvbmVudCB9IGZyb20gXCJAYW5ndWxhci9jb3JlXCI7XG5cbkBDb21wb25lbnQoe1xuICBzZWxlY3RvcjogXCJteS1hcHBcIixcbiAgdGVtcGxhdGU6IGBcbiAgPFRleHRGaWVsZCBoaW50PVwiRW1haWwgQWRkcmVzc1wiIGtleWJvYXJkVHlwZT1cImVtYWlsXCJcbiAgYXV0b2NvcnJlY3Q9XCJmYWxzZVwiIGF1dG9jYXBpdGFsaXphdGlvblR5cGU9XCJub25lXCI+PC9UZXh0RmllbGQ+XG4gIDxUZXh0RmllbGQgaGludD1cIlBhc3N3b3JkXCIgc2VjdXJlPVwidHJ1ZVwiPjwvVGV4dEZpZWxkPlxuICBcbiAgPEJ1dHRvbiB0ZXh0PVwiU2lnbiBpblwiPjwvQnV0dG9uPlxuICA8QnV0dG9uIHRleHQ9XCJTaWduIHVwIGZvciBHcm9jZXJpZXNcIj48PC9CdXR0b24+XG5cbiAgYFxufSlcbmV4cG9ydCBjbGFzcyBBcHBDb21wb25lbnQge30iXX0=
