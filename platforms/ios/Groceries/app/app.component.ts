086a0645fbc45cdf4b1d850b0d78e9b93a9532ed
rkanjani <rkanj017@uottawa.ca>
2017-02-07 23:02:08 -0500

    small change

--- platforms/ios/Groceries/app/app.component.ts
+++ platforms/ios/Groceries/app/app.component.ts
@@ -7,11 +7,11 @@ import { Component } from "@angular/core";
     <TextField hint="Email Address" keyboardType="email"
     autocorrect="false" autocapitalizationType="none"></TextField>
     <TextField hint="Password" secure="true"></TextField>
-    
     <Button text="Sign in"></Button>
     <Button text="Sign up for Groceries"></Button>
   </StackLayout>
 
-  `
+  `,
+  styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
 export class AppComponent {}
